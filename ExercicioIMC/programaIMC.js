e26e8ddc416a094da82bfad660698b92a710f912
sanklerbergman <sanklerbergman@gmail.com>
2021-08-16 22:22:58 -0400

    Create programaIMC.js
    inclusão de calculo IMC v0.0.1

---
+++ ExercicioIMC/programaIMC.js
@@ -0,0 +1,14 @@
+let programaIMC = [
+    nome = 'Jose da Silva',
+    idade = 27,
+    peso = 83.5,
+    altura = 1.70,
+    plano = true,
+    IMC = peso/altura ** 2 
+]
+console.log("Nome: "+programaIMC[0])
+console.log("Idade: "+programaIMC[1])
+console.log("Peso: "+programaIMC[2])
+console.log("Altura: "+programaIMC[3])
+console.log("Plano: "+programaIMC[4])
+console.log("IMC: "+programaIMC[5])
