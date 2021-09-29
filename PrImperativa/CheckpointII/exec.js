e9c68cf98480938ff9981bf83444250b58a2375c
sanklerbergman <sanklerbergman@gmail.com>
2021-09-29 18:37:52 -0400

    up2
    update pc de casa

---
+++ PrImperativa/CheckpointII/exec.js
@@ -0,0 +1,45 @@
+// function soma (a=1, b=3){
+//     return a+b
+// }
+
+// console.log(soma(1,2))
+// let numeros= [10, 8, 7, 99, 0, 1, 6]
+
+// console.log(numeros.sort(function(a,b){ return b-a;}));
+
+// let frutasAmarelas=['melao', 'mamao', 'limao']
+// let [fruta1, fruta2, fruta3]=frutasAmarelas
+
+// console.log(fruta1)
+// console.log(fruta2)
+// console.log(fruta3)
+// console.log(frutasAmarelas)
+
+// * QUESTÃO 13 *
+
+// let x=5
+// let y = x++ + ++x
+// console.log('y= '+y) // essa parte não entendi por que printou 12
+
+// x=3
+// y=x*(x+1)*x++
+// x=5
+// y=3
+// y*=x+1 // o que significa esse sinal de multiplicação na variavel
+// console.log('x= '+x)
+// console.log('y= '+y)
+
+// *             *
+
+//  *  QUESTÃO 7 - ficou uma duvida sobre o que o exercicio pedia, se era um valor certo ou a execução correta    *
+// let valor=5
+// let fatorial=1
+// for (let i = 0; i <=5; i++){
+//     fatorial*=i
+// }
+// console.log('fatorial= '+fatorial)
+
+for (var i=0; i<50; i+=10){
+    console.log(i)
+}
+console.log(i)
